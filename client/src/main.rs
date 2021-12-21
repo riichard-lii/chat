a4c74e1ed52c55f169bdf25527124ab6f85abd74
richard <jsksosdgkd29y@gmail.com>
2021-12-21 15:29:37 -0500

    added a client

---
+++ client/src/main.rs
@@ -0,0 +1,46 @@
+use clap::Parser;
+use std::error::Error;
+use std::io;
+use std::str;
+use tokio::net::TcpStream;
+use tokio::io::{AsyncWriteExt, AsyncReadExt};
+
+#[derive(Parser, Debug)]
+#[clap(name = "chat client", author = "Richard")]
+struct Args {
+    // port number that this client will run on
+    port: i16
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    // let args = Args::parse();
+    // println!("Hello, world! {}", args.port);
+    let stream = TcpStream::connect("127.0.0.1:8888").await?;
+    let (mut read_half, mut write_half) = stream.into_split();
+    println!("Enter your username: ");
+    tokio::spawn(async move {
+        loop {
+            let mut buf = [0; 1024];
+            let msg_size = match read_half.read(&mut buf).await {
+                Ok(msg_size) if msg_size == 0 => return,
+                Ok(msg_size) => msg_size,
+                Err(e) => {
+                    eprintln!("failed to read from socket; err = {:?}", e);
+                    return;
+                }
+            };
+            print!("{}", str::from_utf8(&buf[0..msg_size]).unwrap());
+        }
+    });
+
+    // get username from stdin
+    let mut name = String::new();
+    io::stdin().read_line(&mut name)?;
+    write_half.write_all(name.as_bytes()).await?;
+    loop {
+        let mut name = String::new();
+        io::stdin().read_line(&mut name)?;
+        write_half.write_all(name.as_bytes()).await?;
+    }
+}
